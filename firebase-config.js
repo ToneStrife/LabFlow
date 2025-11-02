6fcc2212018ee8154c3d404865b4da9921d4cb62
ToneStrife <ToneStrife@users.noreply.github.com>
2025-11-02 16:56:43 +0000

    deploy: 0aa58167f60be795627c7102dc2c0371e2e60dd6

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+self.__FIREBASE_CONFIG__ = {
+  apiKey: "YOUR_KEY",
+  authDomain: "labflow-af22c.firebaseapp.com",
+  projectId: "labflow-af22c",
+  storageBucket: "labflow-af22c.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
+};
